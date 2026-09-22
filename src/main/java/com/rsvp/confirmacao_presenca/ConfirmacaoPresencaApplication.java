13eb131cbe6c2cfe3cf6246a33080126d4ea6da3
Loac02 <tiagofs5818@gmail.com>
2026-09-22 18:46:43 -0300

    feat: configuracao inicial do ambiente Spring Boot, Docker e Frontend

---
+++ src/main/java/com/rsvp/confirmacao_presenca/ConfirmacaoPresencaApplication.java
@@ -0,0 +1,13 @@
+package com.rsvp.confirmacao_presenca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfirmacaoPresencaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfirmacaoPresencaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rsvp/confirmacao_presenca/ConfirmacaoPresencaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rsvp.confirmacao_presenca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfirmacaoPresencaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
